471fec9ee819b22f95a98ef88e3e5baf8b2d56bf
Ebby Chakkungal <121340114+EbbyChack@users.noreply.github.com>
2024-01-30 14:50:06 +0100

    ok

---
+++ 30gennaioEs/Persona.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace _30gennaioEs
+{
+	public class Persona
+	{
+	
+		public string nome { get; set; }
+		public string cognome { get; set; }
+        public int età { get; set; }
+
+		public string getNome()
+		{
+			return nome;
+		}
+
+        public string getCognome()
+        {
+            return cognome;
+        }
+
+        public int getEtà()
+        {
+            return età;
+        }
+
+        public string getDettagli()
+        {
+            return (nome +" "+ cognome+ " ha "+età+" anni");
+        }
+    }
+}
+
---
+++ 30gennaioEs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _30gennaioEs {
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona();
+            persona1.nome = "Ebby";
+            persona1.cognome = "Chakkungal";
+            persona1.età = 23;
+
+            Console.WriteLine(persona1.getDettagli());
+            Console.ReadLine();
+        }
+    }
+}
+
